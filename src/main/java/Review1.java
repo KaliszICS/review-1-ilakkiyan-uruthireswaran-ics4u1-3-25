3d1619b667f6aa01f596d777d266d458a00b154b
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-02-05 17:52:46 +0000

    commit

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,43 @@
 public class Review1 {
 
+	public static void main(String[] args) {
+		// runs different questions from q1 to q5
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
+
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int x = 9;
+		int y = 5;
+		System.out.println(x/y);
+		System.out.println(x%y);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean Good = true;
+		System.out.println(Good);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String First = "Hello";
+		String Second = "World";
+		System.out.println(First + " " + Second);
 	}
 
 }
